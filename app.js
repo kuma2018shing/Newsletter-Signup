8ff8d669547da8d4ef80b7f96d970c165cc5cb9b
kuma lawrence shing <52683208+kuma2018shing@users.noreply.github.com>
2020-01-05 14:43:35 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion:6
+
+const express  = require("express");
+const bodyParser  = require("body-parser");
+const request  = require("request");
+
+
+const app = express();
+
+//serving static files ie from local
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+//body api member reference (object)
+  var data = {
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields: {
+          FNAME:firstName,
+          LNAME:lastName
+        }
+      }
+    ]
+  };
+//
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    //Authorization post request
+    url:"https://us4.api.mailchimp.com/3.0/lists/9172d5e8d7",
+    method:"POST",
+    headers:{
+      "Authorization":"kuma 8364e6aeae2afcf6b9136ebc02c4bbf0-us4"
+    },
+    //body data*
+    body:jsonData
+  };
+  request(options, function(error, response, body){
+    //message confirmation
+    if(error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if(response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+      }else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+  // console.log(firstName, lastName, email);
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000.");
+});
+
+
+// 8364e6aeae2afcf6b9136ebc02c4bbf0-us4
+// 9172d5e8d7
